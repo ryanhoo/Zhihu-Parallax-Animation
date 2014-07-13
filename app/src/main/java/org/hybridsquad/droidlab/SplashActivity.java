5c71d20b3f234687737d747718849a3b5dc2b12d
ryanhoo <ryan.hoo.j@gmail.com>
2014-07-13 18:27:15 +0800

    chore: update gradle package name

--- app/src/main/java/org/hybridsquad/droidlab/SplashActivity.java
+++ app/src/main/java/org/hybridsquad/droidlab/SplashActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import droidlab.hybridsquad.org.zhihu_parallax_animation.R;
 
 
 public class SplashActivity extends ActionBarActivity {
