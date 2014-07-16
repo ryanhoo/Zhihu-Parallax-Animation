cb4e374dd41624e6d108b95a3cca8398d4b5799b
ryanhoo <ryan.hoo.j@gmail.com>
2014-07-16 15:35:08 +0800

    feat(anim): add animation for guide page first

--- app/src/main/java/org/hybridsquad/droidlab/FirstGuideFragment.java
+++ app/src/main/java/org/hybridsquad/droidlab/FirstGuideFragment.java
@@ -1,9 +1,12 @@
 package org.hybridsquad.droidlab;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 /**
  * Created with Android Studio.
@@ -14,9 +17,31 @@ import android.view.ViewGroup;
  */
 public class FirstGuideFragment extends BaseGuideFragment {
 
+    final long ANIMATION_DURATION = 500;
+    final long ANIMATION_OFFSET = 200;
+
+    private int[] mAnimationViewIds = {
+            R.id.guide_item_2, R.id.guide_item_3, R.id.guide_item_4,
+            R.id.guide_item_5, R.id.guide_item_6, R.id.guide_item_7,
+            R.id.guide_item_8, R.id.guide_item_9, R.id.guide_item_10,
+            R.id.guide_item_11, R.id.guide_item_12, R.id.guide_item_13
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_guide_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        for (int i = 0; i < mAnimationViewIds.length; i++) {
+            Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.splash_guide_items);
+            animation.setDuration(ANIMATION_DURATION);
+            animation.setStartOffset(ANIMATION_OFFSET * i);
+            view.findViewById(mAnimationViewIds[i]).startAnimation(animation);
+        }
     }
 
     @Override
