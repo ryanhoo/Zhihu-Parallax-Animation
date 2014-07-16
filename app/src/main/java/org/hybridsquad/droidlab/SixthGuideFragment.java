bb32aa5e8093e65b948f56c0b250926cfab8b64a
ryanhoo <ryan.hoo.j@gmail.com>
2014-07-16 15:13:54 +0800

    feat(anim): add animation for guide page sixth

--- app/src/main/java/org/hybridsquad/droidlab/SixthGuideFragment.java
+++ app/src/main/java/org/hybridsquad/droidlab/SixthGuideFragment.java
@@ -1,9 +1,13 @@
 package org.hybridsquad.droidlab;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 
 /**
  * Created with Android Studio.
@@ -14,9 +18,21 @@ import android.view.ViewGroup;
  */
 public class SixthGuideFragment extends BaseGuideFragment {
 
+    View mLayoutLogo;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_guide_sixth, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mLayoutLogo = view.findViewById(R.id.guide_item_1);
+
+        Animation anim = AnimationUtils.loadAnimation(getActivity(), R.anim.splash_guide_logo);
+        mLayoutLogo.startAnimation(anim);
     }
 
     @Override
